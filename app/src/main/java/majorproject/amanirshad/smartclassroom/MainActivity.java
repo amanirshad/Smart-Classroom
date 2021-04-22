5786d035c0b393c78d247bd02afea1cc24b82102
amanirshad <aman.itz.me07@gmail.com>
2021-04-22 15:41:53 +0530

    feat: added image slider

--- app/src/main/java/majorproject/amanirshad/smartclassroom/MainActivity.java
+++ app/src/main/java/majorproject/amanirshad/smartclassroom/MainActivity.java
@@ -3,10 +3,14 @@ package majorproject.amanirshad.smartclassroom;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.ImageView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.smarteist.autoimageslider.DefaultSliderView;
+import com.smarteist.autoimageslider.IndicatorAnimations;
+import com.smarteist.autoimageslider.SliderLayout;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -20,44 +24,46 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
 
+    private SliderLayout sliderLayout;
+    private final int IMAGE_SCROLL_TIME = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+        //Working of Image Slider
+        sliderLayout = findViewById(R.id.imageslider);
+        sliderLayout.setIndicatorAnimation(IndicatorAnimations.WORM);
+        sliderLayout.setScrollTimeInSec(IMAGE_SCROLL_TIME);
+        setSliderView();
+
+    }
+
+    /***
+     * This function defines the images in the Image Slider
+     */
+    private void setSliderView() {
+        for (int i =0; i<3; i++){
+            DefaultSliderView sliderView = new DefaultSliderView(this);
+
+            switch (i){
+                case 0:
+                    sliderView.setImageDrawable(R.drawable.picture_01);
+                    break;
+                case 1:
+                    sliderView.setImageDrawable(R.drawable.picture_02);
+                    break;
+                case 2:
+                    sliderView.setImageDrawable(R.drawable.picture_03);
+                    break;
             }
-        });
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+
+            sliderView.setImageScaleType(ImageView.ScaleType.FIT_CENTER);
+
+            //at last add this view to your layout
+            sliderLayout.addSliderView(sliderView);
+        }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
 }
