945c6e1656aebf57778b7b5570ff062adf7d326a
amanirshad <aman.itz.me07@gmail.com>
2021-04-24 15:35:07 +0530

    feat: Added toolbar and initialization of DrawerLayout(75%)

--- app/src/main/java/majorproject/amanirshad/smartclassroom/MainActivity.java
+++ app/src/main/java/majorproject/amanirshad/smartclassroom/MainActivity.java
@@ -1,10 +1,14 @@
 package majorproject.amanirshad.smartclassroom;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 import android.widget.ImageView;
 
+import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -12,6 +16,9 @@ import com.smarteist.autoimageslider.DefaultSliderView;
 import com.smarteist.autoimageslider.IndicatorAnimations;
 import com.smarteist.autoimageslider.SliderLayout;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -22,7 +29,11 @@ import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration mAppBarConfiguration;
+    private Toolbar mToolbar;
+
+    private DrawerLayout mDrawerLayout;
+
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
 
     private SliderLayout sliderLayout;
     private final int IMAGE_SCROLL_TIME = 1;
@@ -32,12 +43,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Initialization of DrawerLayout
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        setToolbar();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        mDrawerLayout.setDrawerListener(mActionBarDrawerToggle);
+        setupDrawerToggle();
+
+
         //Working of Image Slider
         sliderLayout = findViewById(R.id.imageslider);
         sliderLayout.setIndicatorAnimation(IndicatorAnimations.WORM);
         sliderLayout.setScrollTimeInSec(IMAGE_SCROLL_TIME);
         setSliderView();
 
+    }
+
+    private void setupDrawerToggle() {
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.app_name,R.string.app_name);
+        mActionBarDrawerToggle.syncState();
+    }
+
+    private void setToolbar() {
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     /***
@@ -66,4 +98,18 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (mActionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onPostCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onPostCreate(savedInstanceState, persistentState);
+        mActionBarDrawerToggle.syncState();
+    }
 }
